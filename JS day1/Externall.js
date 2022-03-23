7797263cb8b0d309b635c6aafe11802d57f7487d
unknown <ahmedhamdyyehia@gmail.com>
2022-03-23 03:56:24 +0200

    upload js demos

---
+++ JS day1/Externall.js
@@ -0,0 +1,11 @@
+var number1=3;
+var number2=2.9;
+var number3=90;
+var numbers=[10,"string",20];
+var sum;
+var flag=true;
+ firstName="Ahmed";
+ lastName="hamdy";
+
+console.log("This is the External JavaScript file");
+console.log("This is the External JavaScript file");
